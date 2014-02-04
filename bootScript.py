25c082e66e5483de9e5fd8d77b64b46b3cc6fee9
ricky <rickyp72@googmail.com>
2014-02-04 15:08:14 +0000

    no message

--- bootScript.py
+++ bootScript.py
@@ -14,39 +14,25 @@ f = open('/var/log/bootupdate.log', 'w')
 f.write('scripts called\n')
 f.close()
 
-# Get the owner of the file the new user created on firstboot.
+# Get the owner of the file the new user created on firstboot, and apply it to boxen
 def find_owner(filename):
     return getpwuid(stat(filename).st_uid).pw_name
 
 new_user = find_owner('/Users/Shared/newuser.txt')
-# new_uid = pwd.getpwnam(new_user).pw_uid
-# new_gid = grp.getgrnam("wheel").gr_gid
-
-# Open boxen default.json file and change user
-# Change owner of /opt/boxen folder
-# for root, dirs, files in os.walk('/opt/boxen'):
-# 	for momo in dirs:
-# 		os.chown(os.path.join(root, momo), new_uid, new_gid)
-# 	for momo in files:
-# 		os.chown(os.path.join(root, momo), new_uid, new_gid)
 subprocess.call(['chown', '-R', new_user, '/opt/boxen'])
 
-# os.chown('/opt/boxen', new_uid, new_gid)
 
-# check for software updates
+# Update apple software
 subprocess.call(['softwareupdate', '-ia'])
-
 now = time.strftime("%c")
 
 
-
+# Log some stuff
 f = open('/var/log/bootupdate.log', 'a')
 f.write("Softwareupdate -ia command ran %s" % now)
 f.close()
 
 f = open('/var/log/bootupdate.log', 'a')
-
-
-# f.write(str(current_user))
 f.write(new_user)
+f.close()
 
