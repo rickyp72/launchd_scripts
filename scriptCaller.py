43cffd20f13502f543483add48d01f6c8dbb00d1
ricky <rickyp72@googmail.com>
2014-02-04 09:02:53 +0000

    no message

--- scriptCaller.py
+++ scriptCaller.py
@@ -11,6 +11,14 @@ script_path = "/Users/" + current_user + "/restartMac.py"
 # Wait 
 time.sleep(6)
 
+if os.path.isfile(script_path):
+	temp_userfile = open('/tmp/domainuser', 'r')
+	newuser = temp_userfile.read()
+	temp_userfile.close()
+	f = open('/var/log/newuser.log', 'w')
+	f.write(str(newuser))
+	f.close()
+
 # Call user's restart script
 subprocess.call(['python', script_path])
 
