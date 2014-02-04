acd1533c583203979bb9c07b0e8056adb7677ea4
ricky <rickyp72@googmail.com>
2014-02-04 14:34:55 +0000

    no message

--- bootScript.py
+++ bootScript.py
@@ -5,6 +5,8 @@ import time
 import os
 from os import stat
 from pwd import getpwuid
+import pwd
+import grp
 
 #TODO: logic to check if file exists already.
 
@@ -17,10 +19,12 @@ def find_owner(filename):
     return getpwuid(stat(filename).st_uid).pw_name
 
 new_user = find_owner('/Users/Shared/newuser.txt')
+new_uid = pwd.getpwnam(new_user).pw_uid
+new_gid = grp.getgrnam("wheel").gr_gid
 
 # Open boxen default.json file and change user
 # Change owner of /opt/boxen folder
-
+os.chown('/opt/boxen', new_uid, new_gid)
 
 # check for software updates
 subprocess.call(['softwareupdate', '-ia'])
